73bd1dd1aff9bd28c58e7e93a6cff43c9431de91
찬기 <69755603+Lim-Changi@users.noreply.github.com>
2024-03-12 02:15:04 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 12.29 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,67 @@
+function solution(bandage, health, attacks) {
+    var answer = 0;
+    const attackCount = attacks.length
+    const lastAttackTime = attacks[attackCount - 1][0];
+    
+    let currentHealth = health;
+    let contiHealCount = 0;
+    let timeHeal = bandage[1];
+    const bonusHeal = bandage[2];
+    
+    const checkDeath = (currentHealth, damage) => {
+        if (currentHealth - damage <= 0) return true
+    }
+    const damage = (time, attacks) => {
+        if (attacks[0][0] == time) {
+            const attack = attacks.shift()
+            return attack[1];
+        } else {
+            return 0
+        }
+    }
+    const heal = (currentHealth, healMount, max) => {
+        if (currentHealth + healMount >= max) {
+            return max;
+        } else {
+            return currentHealth + healMount
+        }
+    }
+    const contiHealCheck = (damage) => {
+        if (damage == 0) {
+            contiHealCount++;
+            if (contiHealCount == bandage[0]){
+                contiHealCount = 0;
+                return bonusHeal;
+            }
+            return 0;
+        } else {
+            contiHealCount = 0;
+            return 0;
+        }
+    }
+    
+    
+    for (let time = 0; time <= lastAttackTime; time++) {
+        const timeDamage = damage(time, attacks);
+        if(checkDeath(currentHealth, timeDamage)) {
+            answer = -1;
+            break;
+        }
+        if (timeDamage > 0) {
+            currentHealth -= timeDamage;
+            contiHealCount = 0;
+            console.log(currentHealth)
+            continue;
+        }
+        
+        const bonusHealMount = contiHealCheck(timeDamage);
+        currentHealth = heal(currentHealth, timeHeal + bonusHealMount, health)
+        console.log(currentHealth)
+    }
+    if(answer == 0) {
+        answer = currentHealth;
+    }
+    
+    
+    return answer;
+}
